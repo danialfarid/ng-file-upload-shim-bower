0d2b0879aa9777eebb430d37bc9735b0d43261b0
Dan <dan@Danial.local>
2014-05-08 19:17:20 -0400

    1.4.0

--- angular-file-upload-shim.js
+++ angular-file-upload-shim.js
@@ -1,139 +1,157 @@
 /**!
  * AngularJS file upload shim for HTML5 FormData
  * @author  Danial  <danial.farid@gmail.com>
- * @version 1.2.3
+ * @version 1.4.0
  */
 (function() {
+
+var hasFlash = function() {
+	try {
+	  var fo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+	  if (fo) return true;
+	} catch(e) {
+	  if (navigator.mimeTypes["application/x-shockwave-flash"] != undefined) return true;
+	}
+	return false;
+}
+
+var patchXHR = function(fnName, newFn) {
+	window.XMLHttpRequest.prototype[fnName] = newFn(window.XMLHttpRequest.prototype[fnName]);
+};
 
 if (window.XMLHttpRequest) {
 	if (window.FormData) {
 		// allow access to Angular XHR private field: https://github.com/angular/angular.js/issues/1934
-		window.XMLHttpRequest = (function(origXHR) {
-			return function() {
-				var xhr = new origXHR();
-				xhr.setRequestHeader = (function(orig) {
-					return function(header, value) {
-						if (header === '__setXHR_') {
-							var val = value(xhr);
-							// fix for angular < 1.2.0
-							if (val instanceof Function) {
-								val(xhr);
-							}
-						} else {
-							orig.apply(xhr, arguments);
-						}
+		patchXHR("setRequestHeader", function(orig) {
+			return function(header, value) {
+				if (header === '__setXHR_') {
+					var val = value(this);
+					// fix for angular < 1.2.0
+					if (val instanceof Function) {
+						val(this);
 					}
-				})(xhr.setRequestHeader);
-				return xhr;
-			}
-		})(window.XMLHttpRequest);
-	} else {
-		window.XMLHttpRequest = (function(origXHR) {
-			return function() {
-				var xhr = new origXHR();
-				var origSend = xhr.send;
-				xhr.__requestHeaders = [];
-				xhr.open = (function(orig) {
-					xhr.upload = {
-						addEventListener: function(t, fn, b) {
-							if (t === 'progress') {
-								xhr.__progress = fn;
-							}
-							if (t === 'load') {
-								xhr.__load = fn;
-							}
-						}
-					};
-					return function(m, url, b) {
-						orig.apply(xhr, [m, url, b]);
-						xhr.__url = url;
-					}
-				})(xhr.open);
-				xhr.getResponseHeader = (function(orig) {
-					return function(h) {
-						return xhr.__fileApiXHR ? xhr.__fileApiXHR.getResponseHeader(h) : orig.apply(xhr, [h]); 
-					}
-				})(xhr.getResponseHeader);
-				xhr.getAllResponseHeaders = (function(orig) {
-					return function() {
-						return xhr.__fileApiXHR ? xhr.__fileApiXHR.getAllResponseHeaders() : orig.apply(xhr); 
-					}
-				})(xhr.getAllResponseHeaders);
-				xhr.abort = (function(orig) {
-					return function() {
-						return xhr.__fileApiXHR ? xhr.__fileApiXHR.abort() : (orig == null ? null : orig.apply(xhr)); 
-					}
-				})(xhr.abort);
-				xhr.setRequestHeader = (function(orig) {
-					return function(header, value) {
-						if (header === '__setXHR_') {
-							var val = value(xhr);
-							// fix for angular < 1.2.0
-							if (val instanceof Function) {
-								val(xhr);
-							}
-						} else {
-							orig.apply(xhr, arguments);
-						}
-					}
-				})(xhr.setRequestHeader);
-			
-				xhr.send = function() {
-					if (arguments[0] && arguments[0].__isShim) {
-						var formData = arguments[0];
-						var config = {
-							url: xhr.__url,
-							complete: function(err, fileApiXHR) {
-								xhr.__load({type: 'load', loaded: xhr.__total, total: xhr.__total, target: xhr, lengthComputable: true});
-								Object.defineProperty(xhr, 'status', {get: function() {return fileApiXHR.status}});
-								Object.defineProperty(xhr, 'statusText', {get: function() {return fileApiXHR.statusText}});
-								Object.defineProperty(xhr, 'readyState', {get: function() {return 4}});
-								Object.defineProperty(xhr, 'response', {get: function() {return fileApiXHR.response}});
-								Object.defineProperty(xhr, 'responseText', {get: function() {return fileApiXHR.responseText}});
-								xhr.__fileApiXHR = fileApiXHR;
-								xhr.onreadystatechange();
-							},
-							progress: function(e) {
-								e.target = xhr;
-								xhr.__progress(e);
-								xhr.__total = e.total;
-							},
-							headers: xhr.__requestHeaders
-						}
-						config.data = {};
-						config.files = {}
-						for (var i = 0; i < formData.data.length; i++) {
-							var item = formData.data[i];
-							if (item.val != null && item.val.name != null && item.val.size != null && item.val.type != null) {
-								config.files[item.key] = item.val;
-							} else {
-								config.data[item.key] = item.val;
-							}
-						}
-						
-						setTimeout(function() {
-							xhr.__fileApiXHR = FileAPI.upload(config);
-						}, 1);
-					} else {
-						origSend.apply(xhr, arguments);
-					}
+				} else {
+					orig.apply(this, arguments);
 				}
-				return xhr;
 			}
-		})(window.XMLHttpRequest);
+		});
+	} else {
+		function initializeUploadListener(xhr) {
+			if (!xhr.__listeners) {
+				if (!xhr.upload) xhr.upload = {};
+				xhr.__listeners = [];
+				var origAddEventListener = xhr.upload.addEventListener;
+				xhr.upload.addEventListener = function(t, fn, b) {
+					xhr.__listeners[t] = fn;
+					origAddEventListener && origAddEventListener.apply(this, arguments);
+				};
+			}
+		}
+		
+		patchXHR("open", function(orig) {
+			return function(m, url, b) {
+				initializeUploadListener(this);
+				this.__url = url;
+				orig.apply(this, [m, url, b]);
+			}
+		});
+
+		patchXHR("getResponseHeader", function(orig) {
+			return function(h) {
+				return this.__fileApiXHR ? this.__fileApiXHR.getResponseHeader(h) : orig.apply(this, [h]);
+			};
+		});
+
+		patchXHR("getAllResponseHeaders", function(orig) {
+			return function() {
+				return this.__fileApiXHR ? this.__fileApiXHR.abort() : (orig == null ? null : orig.apply(this));
+			}
+		});
+
+		patchXHR("abort", function(orig) {
+			return function() {
+				return this.__fileApiXHR ? this.__fileApiXHR.abort() : (orig == null ? null : orig.apply(this));
+			}
+		});
+
+		patchXHR("setRequestHeader", function(orig) {
+			return function(header, value) {
+				if (header === '__setXHR_') {
+					initializeUploadListener(this);
+					var val = value(this);
+					// fix for angular < 1.2.0
+					if (val instanceof Function) {
+						val(this);
+					}
+				} else {
+					this.__requestHeaders = this.__requestHeaders || {};
+					this.__requestHeaders[header] = value;
+					orig.apply(this, arguments);
+				}
+			}
+		});
+
+		patchXHR("send", function(orig) {
+			return function() {
+				var xhr = this;
+				if (arguments[0] && arguments[0].__isShim) {
+					var formData = arguments[0];
+					var config = {
+						url: xhr.__url,
+						complete: function(err, fileApiXHR) {
+							if (!err && xhr.__listeners['load']) 
+								xhr.__listeners['load']({type: 'load', loaded: xhr.__loaded, total: xhr.__total, target: xhr, lengthComputable: true});
+							if (!err && xhr.__listeners['loadend']) 
+								xhr.__listeners['loadend']({type: 'loadend', loaded: xhr.__loaded, total: xhr.__total, target: xhr, lengthComputable: true});
+							if (err === 'abort' && xhr.__listeners['abort']) 
+								xhr.__listeners['abort']({type: 'abort', loaded: xhr.__loaded, total: xhr.__total, target: xhr, lengthComputable: true});
+							if (fileApiXHR.status !== undefined) Object.defineProperty(xhr, 'status', {get: function() {return fileApiXHR.status}});
+							if (fileApiXHR.statusText !== undefined) Object.defineProperty(xhr, 'statusText', {get: function() {return fileApiXHR.statusText}});
+							Object.defineProperty(xhr, 'readyState', {get: function() {return 4}});
+							if (fileApiXHR.response !== undefined) Object.defineProperty(xhr, 'response', {get: function() {return fileApiXHR.response}});
+							Object.defineProperty(xhr, 'responseText', {get: function() {return fileApiXHR.responseText}});
+							xhr.__fileApiXHR = fileApiXHR;
+							xhr.onreadystatechange();
+						},
+						fileprogress: function(e) {
+							e.target = xhr;
+							xhr.__listeners['progress'] && xhr.__listeners['progress'](e);
+							xhr.__total = e.total;
+							xhr.__loaded = e.loaded;
+						},
+						headers: xhr.__requestHeaders
+					}
+					config.data = {};
+					config.files = {}
+					for (var i = 0; i < formData.data.length; i++) {
+						var item = formData.data[i];
+						if (item.val != null && item.val.name != null && item.val.size != null && item.val.type != null) {
+							config.files[item.key] = item.val;
+						} else {
+							config.data[item.key] = item.val;
+						}
+					}
+
+					setTimeout(function() {
+						if (!hasFlash()) {
+							throw 'Adode Flash Player need to be installed. To check ahead use "FileAPI.hasFlash"';
+						}
+						xhr.__fileApiXHR = FileAPI.upload(config);
+					}, 1);
+				} else {
+					orig.apply(xhr, arguments);
+				}
+			}
+		});
 	}
 	window.XMLHttpRequest.__isShim = true;
 }
 
 if (!window.FormData) {
-	var hasFlash = false;
-	try {
-	  var fo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
-	  if (fo) hasFlash = true;
-	} catch(e) {
-	  if (navigator.mimeTypes["application/x-shockwave-flash"] != undefined) hasFlash = true;
-	}
 	var wrapFileApi = function(elem) {
+		if (!hasFlash()) {
+			throw 'Adode Flash Player need to be installed. To check ahead use "FileAPI.hasFlash"';
+		}
 		if (!elem.__isWrapped && (elem.getAttribute('ng-file-select') != null || elem.getAttribute('data-ng-file-select') != null)) {
 			var wrap = document.createElement('div');
 			wrap.innerHTML = '<div class="js-fileapi-wrapper" style="position:relative; overflow:hidden"></div>';
@@ -142,9 +160,6 @@ if (!window.FormData) {
 			parent.insertBefore(wrap, elem);
 			parent.removeChild(elem);
 			wrap.appendChild(elem);
-			if (!hasFlash) {
-				wrap.appendChild(document.createTextNode('Flash is required'));
-			}
 			elem.__isWrapped = true;
 		}
 	};
@@ -169,19 +184,19 @@ if (!window.FormData) {
 			return function(e, fn, b, d) {
 				if (isFileChange(this, e)) {
 					wrapFileApi(this);
-					origAddEventListener.apply(this, [e, changeFnWrapper(fn), b, d]); 
+					origAddEventListener.apply(this, [e, changeFnWrapper(fn), b, d]);
 				} else {
 					origAddEventListener.apply(this, [e, fn, b, d]);
 				}
 			}
-		})(HTMLInputElement.prototype.addEventListener);		
+		})(HTMLInputElement.prototype.addEventListener);
 	}
 	if (HTMLInputElement.prototype.attachEvent) {
 		HTMLInputElement.prototype.attachEvent = (function(origAttachEvent) {
 			return function(e, fn) {
 				if (isFileChange(this, e)) {
 					wrapFileApi(this);
-					origAttachEvent.apply(this, [e, changeFnWrapper(fn)]); 
+					origAttachEvent.apply(this, [e, changeFnWrapper(fn)]);
 				} else {
 					origAttachEvent.apply(this, [e, fn]);
 				}
@@ -202,13 +217,18 @@ if (!window.FormData) {
 			__isShim: true
 		};
 	};
-	
+
 	(function () {
 		//load FileAPI
-		if (!window.FileAPI || !FileAPI.upload) {
-			var base = '', script = document.createElement('script'), allScripts = document.getElementsByTagName('script'), i, index, src;
-			if (window.FileAPI && window.FileAPI.jsPath) {
-				base = window.FileAPI.jsPath;
+		if (!window.FileAPI) {
+			window.FileAPI = {};
+		}
+		if (!FileAPI.upload) {
+			var jsUrl, basePath, script = document.createElement('script'), allScripts = document.getElementsByTagName('script'), i, index, src;
+			if (window.FileAPI.jsUrl) {
+				jsUrl = window.FileAPI.jsUrl;
+			} else if (window.FileAPI.jsPath) {
+				basePath = window.FileAPI.jsPath;
 			} else {
 				for (i = 0; i < allScripts.length; i++) {
 					src = allScripts[i].src;
@@ -217,31 +237,27 @@ if (!window.FormData) {
 						index = src.indexOf('angular-file-upload-shim.min.js');
 					}
 					if (index > -1) {
-						base = src.substring(0, index);
+						basePath = src.substring(0, index);
 						break;
 					}
 				}
 			}
 
-			if (!window.FileAPI || FileAPI.staticPath == null) {
-				FileAPI = {
-					staticPath: base
-				}
-			}
-	
-			script.setAttribute('src', base + "FileAPI.min.js");
+			if (FileAPI.staticPath == null) FileAPI.staticPath = basePath;
+			script.setAttribute('src', jsUrl || basePath + "FileAPI.min.js");
 			document.getElementsByTagName('head')[0].appendChild(script);
+			FileAPI.hasFlash = hasFlash();
 		}
 	})();
 }
 
-/*
+
 if (!window.FileReader) {
 	window.FileReader = function() {
 		var _this = this, loadStarted = false;
 		this.listeners = {};
 		this.addEventListener = function(type, fn) {
-			_this.listeners[type] = _this.listeners[type] || []; 
+			_this.listeners[type] = _this.listeners[type] || [];
 			_this.listeners[type].push(fn);
 		};
 		this.removeEventListener = function(type, fn) {
@@ -256,7 +272,7 @@ if (!window.FileReader) {
 			}
 		};
 		this.onabort = this.onerror = this.onload = this.onloadstart = this.onloadend = this.onprogress = null;
-		
+
 		function constructEvent(type, evt) {
 			var e = {type: type, target: _this, loaded: evt.loaded, total: evt.total, error: evt.error};
 			if (evt.result != null) e.target.result = evt.result;
@@ -283,7 +299,7 @@ if (!window.FileReader) {
 			}
 		};
 		this.readAsArrayBuffer = function(file) {
-			FileAPI.readAsArrayBuffer(file, listener);
+			FileAPI.readAsBinaryString(file, listener);
 		}
 		this.readAsBinaryString = function(file) {
 			FileAPI.readAsBinaryString(file, listener);
@@ -296,5 +312,5 @@ if (!window.FileReader) {
 		}
 	}
 }
-*/
+
 })();
--- angular-file-upload.js
+++ angular-file-upload.js
@@ -1,52 +1,52 @@
 /**!
  * AngularJS file upload/drop directive with http post and progress
  * @author  Danial  <danial.farid@gmail.com>
- * @version 1.2.3
+ * @version 1.4.0
  */
 (function() {
 	
 var angularFileUpload = angular.module('angularFileUpload', []);
 
-angularFileUpload.service('$upload', ['$http', '$rootScope', '$timeout', function($http, $rootScope, $timeout) {
+angularFileUpload.service('$upload', ['$http', '$timeout', function($http, $timeout) {
 	function sendHttp(config) {
 		config.method = config.method || 'POST';
 		config.headers = config.headers || {};
-		config.transformRequest = config.transformRequest || function(data) {
-			if (window.ArrayBuffer && data instanceof ArrayBuffer) {
+		config.transformRequest = config.transformRequest || function(data, headersGetter) {
+			if (window.ArrayBuffer && data instanceof window.ArrayBuffer) {
 				return data;
 			}
-			return $http.defaults.transformRequest[0](data);
+			return $http.defaults.transformRequest[0](data, headersGetter);
 		};
-		
+
 		if (window.XMLHttpRequest.__isShim) {
 			config.headers['__setXHR_'] = function() {
 				return function(xhr) {
+					if (!xhr) return;
 					config.__XHR = xhr;
+					config.xhrFn && config.xhrFn(xhr);
 					xhr.upload.addEventListener('progress', function(e) {
 						if (config.progress) {
 							$timeout(function() {
-								config.progress(e);
+								if(config.progress) config.progress(e);
 							});
 						}
 					}, false);
 					//fix for firefox not firing upload progress end, also IE8-9
 					xhr.upload.addEventListener('load', function(e) {
 						if (e.lengthComputable) {
-							$timeout(function() {
-								config.progress(e);
-							});
+							if(config.progress) config.progress(e);
 						}
 					}, false);
-				}
+				};
 			};
 		}
-			
+
 		var promise = $http(config);
-		
+
 		promise.progress = function(fn) {
 			config.progress = fn;
 			return promise;
-		};		
+		};
 		promise.abort = function() {
 			if (config.__XHR) {
 				$timeout(function() {
@@ -54,64 +54,84 @@ angularFileUpload.service('$upload', ['$http', '$rootScope', '$timeout', functio
 				});
 			}
 			return promise;
-		};		
+		};
+		promise.xhr = function(fn) {
+			config.xhrFn = fn;
+			return promise;
+		};
 		promise.then = (function(promise, origThen) {
 			return function(s, e, p) {
 				config.progress = p || config.progress;
-				origThen.apply(promise, [s, e, p]);
-				return promise;
+				var result = origThen.apply(promise, [s, e, p]);
+				result.abort = promise.abort;
+				result.progress = promise.progress;
+				result.xhr = promise.xhr;
+				result.then = promise.then;
+				return result;
 			};
 		})(promise, promise.then);
 		
 		return promise;
-	};
+	}
+
 	this.upload = function(config) {
 		config.headers = config.headers || {};
 		config.headers['Content-Type'] = undefined;
 		config.transformRequest = config.transformRequest || $http.defaults.transformRequest;
 		var formData = new FormData();
-		if (config.data) {
-			for (var key in config.data) {
-				var val = config.data[key];
-				if (!config.formDataAppender) {
-					if (typeof config.transformRequest == 'function') {
-						val = config.transformRequest(val);
-					} else {
-						for (var i = 0; i < config.transformRequest.length; i++) {
-							var fn = config.transformRequest[i];
-							if (typeof fn == 'function') {
-								val = fn(val);
+		var origTransformRequest = config.transformRequest;
+		var origData = config.data;
+		config.transformRequest = function(formData, headerGetter) {
+			if (origData) {
+				if (config.formDataAppender) {
+					for (var key in origData) {
+						var val = origData[key];
+						config.formDataAppender(formData, key, val);
+					}
+				} else {
+					for (var key in origData) {
+						var val = origData[key];
+						if (typeof origTransformRequest == 'function') {
+							val = origTransformRequest(val, headerGetter);
+						} else {
+							for (var i = 0; i < origTransformRequest.length; i++) {
+								var transformFn = origTransformRequest[i];
+								if (typeof transformFn == 'function') {
+									val = transformFn(val, headerGetter);
+								}
 							}
 						}
+						formData.append(key, val);
 					}
-					formData.append(key, val);
-				} else {
-					config.formDataAppender(formData, key, val);
 				}
 			}
-		}
-		config.transformRequest =  angular.identity;
-		
-		var fileFromName = config.fileFormDataName || 'file';
-		
-		if (Object.prototype.toString.call(config.file) === '[object Array]') {
-			for (var i = 0; i < config.file.length; i++) {
-				formData.append(fileFromName + i, config.file[i], config.file[i].name);
+
+			if (config.file != null) {
+				var fileFormName = config.fileFormDataName || 'file';
+
+				if (Object.prototype.toString.call(config.file) === '[object Array]') {
+					var isFileFormNameString = Object.prototype.toString.call(fileFormName) === '[object String]'; 
+					for (var i = 0; i < config.file.length; i++) {
+						formData.append(isFileFormNameString ? fileFormName + i : fileFormName[i], config.file[i], config.file[i].name);
+					}
+				} else {
+					formData.append(fileFormName, config.file, config.file.name);
+				}
 			}
-		} else {
-			formData.append(fileFromName, config.file, config.file.name);
-		}
-		
+			return formData;
+		};
+
 		config.data = formData;
-		
+
 		return sendHttp(config);
 	};
+
 	this.http = function(config) {
 		return sendHttp(config);
 	}
 }]);
 
-angularFileUpload.directive('ngFileSelect', [ '$parse', '$http', '$timeout', function($parse, $http, $timeout) {
+angularFileUpload.directive('ngFileSelect', [ '$parse', '$timeout', function($parse, $timeout) {
 	return function(scope, elem, attr) {
 		var fn = $parse(attr['ngFileSelect']);
 		elem.bind('change', function(evt) {
@@ -129,13 +149,23 @@ angularFileUpload.directive('ngFileSelect', [ '$parse', '$http', '$timeout', fun
 				});
 			});
 		});
-		elem.bind('click', function(){
-			this.value = null;
-		});
+		// removed this since it was confusing if the user click on browse and then cancel #181
+//		elem.bind('click', function(){
+//			this.value = null;
+//		});
+		
+		// touch screens
+		if (('ontouchstart' in window) ||
+				(navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)) {
+			elem.bind('touchend', function(e) {
+				e.preventDefault();
+				e.target.click();
+			});
+		}
 	};
 } ]);
 
-angularFileUpload.directive('ngFileDropAvailable', [ '$parse', '$http', '$timeout', function($parse, $http, $timeout) {
+angularFileUpload.directive('ngFileDropAvailable', [ '$parse', '$timeout', function($parse, $timeout) {
 	return function(scope, elem, attr) {
 		if ('draggable' in document.createElement('span')) {
 			var fn = $parse(attr['ngFileDropAvailable']);
@@ -146,34 +176,72 @@ angularFileUpload.directive('ngFileDropAvailable', [ '$parse', '$http', '$timeou
 	};
 } ]);
 
-angularFileUpload.directive('ngFileDrop', [ '$parse', '$http', '$timeout', function($parse, $http, $timeout) {
-	return function(scope, elem, attr) {
+angularFileUpload.directive('ngFileDrop', [ '$parse', '$timeout', function($parse, $timeout) {
+	return function(scope, elem, attr) {		
 		if ('draggable' in document.createElement('span')) {
+			var cancel = null;
 			var fn = $parse(attr['ngFileDrop']);
 			elem[0].addEventListener("dragover", function(evt) {
+				$timeout.cancel(cancel);
 				evt.stopPropagation();
 				evt.preventDefault();
 				elem.addClass(attr['ngFileDragOverClass'] || "dragover");
 			}, false);
 			elem[0].addEventListener("dragleave", function(evt) {
-				elem.removeClass(attr['ngFileDragOverClass'] || "dragover");
+				cancel = $timeout(function() {
+					elem.removeClass(attr['ngFileDragOverClass'] || "dragover");
+				});
 			}, false);
+			
+			var processing = 0;
+			function traverseFileTree(files, item) {
+				if (item.isDirectory) {
+					var dirReader = item.createReader();
+					processing++;
+					dirReader.readEntries(function(entries) {
+						for (var i = 0; i < entries.length; i++) {
+							traverseFileTree(files, entries[i]);
+						}
+						processing--;
+					});
+				} else {
+					processing++;
+		    	    item.file(function(file) {
+		    	    	processing--;
+		    	    	files.push(file);
+		    	    });
+	    	  }
+			}
+			
 			elem[0].addEventListener("drop", function(evt) {
 				evt.stopPropagation();
 				evt.preventDefault();
 				elem.removeClass(attr['ngFileDragOverClass'] || "dragover");
-				var files = [], fileList = evt.dataTransfer.files, i;
-				if (fileList != null) {
-					for (i = 0; i < fileList.length; i++) {
-						files.push(fileList.item(i));
+				var files = [], items = evt.dataTransfer.items;
+				if (items && items.length > 0 && items[0].webkitGetAsEntry) {
+					for (var i = 0; i < items.length; i++) {
+						traverseFileTree(files, items[i].webkitGetAsEntry());
+					}
+				} else {
+					var fileList = evt.dataTransfer.files;
+					if (fileList != null) {
+						for (var i = 0; i < fileList.length; i++) {
+							files.push(fileList.item(i));
+						}
 					}
 				}
-				$timeout(function() {
-					fn(scope, {
-						$files : files,
-						$event : evt
-					});
-				});
+				(function callback(delay) {
+					$timeout(function() {
+						if (!processing) {
+							fn(scope, {
+								$files : files,
+								$event : evt
+							});
+						} else {
+							callback(10);
+						}
+					}, delay || 0)
+				})();
 			}, false);
 		}
 	};
